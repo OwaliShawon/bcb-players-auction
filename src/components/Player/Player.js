59c3624e443ee25c8354b44a28adcc4f5241bcb5
Owali Ullah Shawon <33037708+OwaliShawon@users.noreply.github.com>
2021-03-01 19:22:11 +0600

    font awesome

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Player.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoneyBillAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Player = (props) => {
     // console.log(props);
@@ -11,7 +13,11 @@ const Player = (props) => {
             <img src={image} alt="" />
             <h3>{first_name} {last_name}</h3>
             <h5>Salary Per Match: ${Math.round(salary)}</h5>
-            <button className={'btn-sm btn-primary'} onClick={() => handleAddPlayer(props.player)}>Add Player</button>
+            <button
+                className={'btn-sm btn-primary'}
+                onClick={() => handleAddPlayer(props.player)}>
+                <FontAwesomeIcon icon={faMoneyBillAlt} /> BidPayment
+                </button>
         </div>
     );
 };
